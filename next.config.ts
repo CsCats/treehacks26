1ed1e975a409dc151e117bf87036de98074b710b
Alex <alex_li@berkeley.edu>
2026-02-14 20:36:29 -0800

    Disable Turbopack, use webpack for production

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove turbopack for production builds
+  // Disable Turbopack for production (use webpack)
+  turbopack: false,
+
   // Optimize output for serverless
   output: 'standalone',
-
-  // Reduce bundle size
-  swcMinify: true,
 
   // Skip type checking during build (faster)
   typescript: {
     ignoreBuildErrors: true,
-  },
-
-  // Skip ESLint during build (faster)
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 
   // Optimize images
@@ -26,21 +20,6 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
       },
     ],
-  },
-
-  // Externalize heavy packages that are loaded via CDN
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Don't bundle TensorFlow/MediaPipe since we load via CDN
-      config.externals = config.externals || {};
-      config.externals = {
-        ...config.externals,
-        '@tensorflow/tfjs': 'tf',
-        '@tensorflow-models/pose-detection': 'poseDetection',
-        '@mediapipe/pose': 'Pose',
-      };
-    }
-    return config;
   },
 };
 
